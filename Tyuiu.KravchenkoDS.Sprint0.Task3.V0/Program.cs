00504fc3c25c45728312c8bc1aa3ed980aa07bb0
skrav <skrav@LAPTOP-GUV9T24N>
2026-09-09 23:52:19 +0500

    Task3

---
+++ Tyuiu.KravchenkoDS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tyuiu.KravchenkoDS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KravchenkoDS.Sprint0.Task3.V0
+{
+    
+
+
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
