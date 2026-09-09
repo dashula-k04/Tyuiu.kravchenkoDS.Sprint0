99592e03e85220f4f58ba947b08faa8a12bb7617
skrav <skrav@LAPTOP-GUV9T24N>
2026-09-09 22:03:03 +0500

    Task2

---
+++ Tyuiu.KravchenkoDS.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.KravchenkoDS.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+       public static string GetMessage(string name)
+        {
+            return $"Привет...., {name}";
+        }
+
+
+    }
+}
---
+++ Tyuiu.KravchenkoDS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KravchenkoDS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KravchenkoDS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Дарья";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет...., Дарья", res);
+            
+        }
+    }
+}
