6f9f3d8386af8d57f4bca7745796bda9aa719ccd
skrav <skrav@LAPTOP-GUV9T24N>
2026-09-10 17:29:40 +0500

    Task4

---
+++ Tyuiu.KravchenkoDS.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.KravchenkoDS.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+      public static int Addition(int a, int b)
+        {  return a + b; }
+    
+        
+            public static int Subtraction(int a, int b)
+            { return a - b; }
+        public static int Multiplication(int a, int b)
+        { return a * b; }
+        public static double Division(int a, int b)
+        { return a / b; }
+
+    }
+}
+
